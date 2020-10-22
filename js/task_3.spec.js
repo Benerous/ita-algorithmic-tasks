3cd9dcfd576ce320254df85923cd75c839950cd1
romanovskyroma <58560068+romanovskyroma@users.noreply.github.com>
2020-10-22 16:49:05 +0300

    allDone

--- js/task_3.spec.js
+++ js/task_3.spec.js
@@ -3,6 +3,8 @@ const {marsennePrime} = require('./task_3');
 test(
     'calculateSecondTask should return number of marsenne',
     () => {
-        expect(marsennePrime(10)).toBe([3, 7]);
+        expect(marsennePrime(10)).toEqual(expect.arrayContaining([3, 7]));
+        expect(marsennePrime(0)).toBe('n is not natural number');
+        expect(marsennePrime(-2)).toBe('n is not natural number');
     }
 )
